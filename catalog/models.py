e34cf40d3c4ab4fdaba7f674b68aa119f73695a0
madyanes <madyanes.1995@gmail.com>
2023-02-04 08:18:36 +0700

    create a model for products

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    expired = models.DateField()
+    stock = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
