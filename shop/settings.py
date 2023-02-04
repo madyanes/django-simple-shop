90f19cd6919094674dbb29d6d20f29988de36ac9
madyanes <madyanes.1995@gmail.com>
2023-02-04 11:37:51 +0700

    integrating bootstrap4 and fontawesome

--- shop/settings.py
+++ shop/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'fontawesomefree',
     'catalog',
 ]
 
