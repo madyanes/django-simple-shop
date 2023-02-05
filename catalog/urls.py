618483556479163a0f4a85b972c2a2fca844df51
madyanes <madyanes.1995@gmail.com>
2023-02-05 11:35:59 +0700

    new feature: delete product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductDeleteView, ProductListView
 
+app_name = 'catalog'  # namespace
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('delete/<uuid:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, ListView
 
 from .models import Product
 
@@ -7,3 +8,9 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'templates/product_list.html'
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'catalog/product_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
