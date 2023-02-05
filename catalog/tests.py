046aebbba118851d35380509388a2cec08b95056
madyanes <madyanes.1995@gmail.com>
2023-02-06 03:50:22 +0700

    simple test as an example only

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CatalogPageTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/catalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('catalog:product_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('catalog:product_list'))
+        self.assertTemplateUsed(response, 'catalog/product_list.html')
