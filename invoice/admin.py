4631ed0aafd542d362bc98e6326b36ef1c0ac66c
madyanes <madyanes.1995@gmail.com>
2023-02-08 16:57:32 +0700

    choice field filtering

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from catalog.models import ProductDetail
 from .models import Invoice, InvoiceItem
 
 class InvoiceItemInline(admin.TabularInline):
@@ -9,5 +10,23 @@ class InvoiceItemInline(admin.TabularInline):
 class InvoiceAdmin(admin.ModelAdmin):
     inlines = [InvoiceItemInline]
 
+    # was trying to filter choice field for Invoice Item to only show the not-binded-yet products with the invoice items
+    # but still failed
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        print(f'isi form============={form.base_fields}')
+        # form.base_fields['product'].queryset = InvoiceItem.objects.filter(product__isnull=True)
+        return form
+
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity', 'price', 'invoice']
+
+    # filter products to show only unbinded products with invoice items
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        print(f'isi form============={form.base_fields}')
+        form.base_fields['product'].queryset = ProductDetail.objects.filter(invoiceitem__isnull=True)
+        return form
+
 admin.site.register(Invoice, InvoiceAdmin)
-admin.site.register(InvoiceItem)
+admin.site.register(InvoiceItem, InvoiceItemAdmin)
