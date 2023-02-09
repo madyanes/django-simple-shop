348ccb6a302bf926e51caf4979a996481fa6d0b2
madyanes <madyanes.1995@gmail.com>
2023-02-09 16:17:28 +0700

    add actions to switch is_active value

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Currency, Product, ProductCategory, ProductDetail, ProductUnit
 
+@admin.action(description='Change to active')
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+@admin.action(description='Change to inactive')
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = ['code', 'name']
+
+class ProductAdmin(admin.ModelAdmin):
+    actions = [make_active, make_inactive]
 
 class ProductDetailAdmin(admin.ModelAdmin):
     list_display = (
@@ -23,7 +34,7 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
 admin.site.register(Currency, CurrencyAdmin)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductCategory, ProductCategoryAdmin)
 admin.site.register(ProductDetail, ProductDetailAdmin)
 admin.site.register(ProductUnit, ProductUnitAdmin)
