a86a7779d3cd391e85bc855d8ec893d919e205f9
madyanes <madyanes.1995@gmail.com>
2023-02-13 19:42:50 +0700

    put api urls inside the project

--- shop/urls.py
+++ shop/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 from rest_framework import routers
 
+from catalog.viewsets import ProductViewSet
 from .views import UserViewSet
 
 # Django Admin Interface (custom)
@@ -28,13 +29,14 @@ admin.site.site_title = 'Simple Shop Administration'
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'products', ProductViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    
+
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 ]
