e880b360a3ff0354c0984d1ff24b62cc38714c61
madyanes <madyanes.1995@gmail.com>
2023-02-13 16:40:57 +0700

    product api

---
+++ catalog/viewsets.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, permissions
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
