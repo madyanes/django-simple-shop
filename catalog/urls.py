eab64135171a2df2e9103501e26866627aae7c8d
madyanes <madyanes.1995@gmail.com>
2023-02-13 19:42:50 +0700

    put api urls inside the project

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,14 +3,9 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import ProductCreateView, ProductDeleteView, ProductListView, ProductUpdateView
-from .viewsets import ProductViewSet
-
-router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
 
 app_name = 'catalog'  # namespace
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('', ProductListView.as_view(), name='product_list'),
     path('delete/<uuid:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('update/<uuid:pk>/', ProductUpdateView.as_view(), name='product_update'),
