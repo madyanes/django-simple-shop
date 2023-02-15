16b22d99f892e0f72ce4292c31ac2d9b7e4399cc
madyanes <madyanes.1995@gmail.com>
2023-02-15 21:35:34 +0700

    refactor model in catalog app

---
+++ catalog/models/product.py
@@ -0,0 +1,26 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    """
+    The products entered to the system should not be deleted.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # model relations
+    category = models.ForeignKey(to='ProductCategory', on_delete=models.PROTECT, null=True)
+    unit = models.ForeignKey(to='ProductUnit', on_delete=models.PROTECT, null=True)
+
+    def get_stock(self) -> int:
+        """
+        Get a product stock.
+        """
+        all_products = self.productdetail_set.all()  # don't forget, a product can have multiple product details
+        return sum([product.stock for product in all_products])
+
+    def __str__(self) -> str:
+        return self.name
